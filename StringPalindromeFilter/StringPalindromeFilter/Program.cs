1666c2ac08cf0c7c8a78783c83c4f17b0c8ab926
Jakov <klaric9jakov@gmail.com>
2020-09-01 14:10:45 +0200

    Finished palindrome filters, started working on anagram filter.

--- StringPalindromeFilter/StringPalindromeFilter/Program.cs
+++ StringPalindromeFilter/StringPalindromeFilter/Program.cs
@@ -8,25 +8,52 @@ namespace StringPalindromeFilter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please enter a word.");
-            var userInput = Console.ReadLine();
+            var inputList = new List<string>();
+            var palindromeResultFirst = "isn't";
+            var palindromeResultSecond = "isn't";
+            var anagramResult = "aren't";
 
-            if (IsPalindrome(userInput))
+            for (var i = 0; inputList.Count < 2; i++)
             {
-                Console.WriteLine("This word is a palindrome.");
+                Console.WriteLine("Please enter a word.");
+                var userInput = Console.ReadLine();
+
+                if (!inputList.Contains(userInput) && !int.TryParse(userInput, out _))
+                {
+                    inputList.Add(userInput);
+                }
+                else
+                {
+                    Console.WriteLine("Please enter a valid word, and make sure it is not the same as the previous one");
+                }
             }
-            else
+
+            if (IsPalindromeFirst(inputList))
             {
-                Console.WriteLine("This word is not a palindrome.");
+                palindromeResultFirst = "is";
             }
+
+            if (IsPalindromeSecond(inputList))
+            {
+                palindromeResultSecond = "is";
+            }
+
+            if (IsAnagram(inputList))
+            {
+
+            }
+
+            Console.WriteLine("The first word " + palindromeResultFirst + 
+                              " a palindromic word. The second word " + palindromeResultSecond + 
+                              " a palindromic word. These words " + anagramResult + " anagrams of each other.");
         }
 
-        static bool IsPalindrome(string userInput)
+        static bool IsPalindromeFirst(List<string> inputList)
         {
             var charList = new List<char>();
             var isPalindromicWord = false;
 
-            foreach (var letter in userInput)
+            foreach (var letter in inputList[0])
             {
                 charList.Add(letter);
             }
@@ -49,6 +76,67 @@ namespace StringPalindromeFilter
             }
 
             return isPalindromicWord;
+        }
+
+        static bool IsPalindromeSecond(List<string> inputList)
+        {
+            var charList = new List<char>();
+            var isPalindromicWord = false;
+
+            foreach (var letter in inputList[1])
+            {
+                charList.Add(letter);
+            }
+
+            for (int i = 0; i < charList.Count; i++)
+            {
+                for (int j = charList.Count - 1; j >= 0; j--)
+                {
+                    if (charList[i] == charList[j])
+                    {
+                        isPalindromicWord = true;
+                    }
+                    else
+                    {
+                        isPalindromicWord = false;
+                    }
+
+                    i++;
+                }
+            }
+
+            return isPalindromicWord;
+        }
+
+        static bool IsAnagram(List<string> inputList)
+        {
+            var firstCharList = new List<char>();
+            var secondCharList = new List<char>();
+            var isWordAnagram = false;
+
+            foreach (var letter in inputList[0])
+            {
+                firstCharList.Add(letter);
+            }
+
+            foreach (var letter in inputList[1])
+            {
+                secondCharList.Add(letter);
+            }
+
+            firstCharList.Sort();
+            secondCharList.Sort();
+
+            if (inputList[0].Length != inputList[1].Length)
+            {
+                isWordAnagram = false;
+            }
+            else if (firstCharList == secondCharList)
+            {
+                isWordAnagram = true;
+            }
+
+            return isWordAnagram;
         }
     }
 }
